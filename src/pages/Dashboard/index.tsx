163ad9c3c41739b673ada0d698540ddd30a98334
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-01 18:20:55 +0900

    refactor: Remove table ver. of list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,9 @@
-import SearchResultTableVer from './_components/SearchResultTable';
 import SearchResult from './_components/SearchResult';
 import styles from './dashboard.module.scss';
 
 function Dashboard() {
   return (
     <div className={styles.tmpWrapper}>
-      <SearchResultTableVer />
       <SearchResult />
     </div>
   );
