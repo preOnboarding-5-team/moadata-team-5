fefebebbf1f5e2de961e47d3d52d75db3ea95cea
taehoon <ender4@naver.com>
2022-06-02 21:22:16 +0900

    refactor: searchFormInput

--- src/pages/UserManagement/SearchForm/SearchFormInput.tsx
+++ src/pages/UserManagement/SearchForm/SearchFormInput.tsx
@@ -1,32 +1,28 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useState, useRef } from 'react';
+import { KeyboardEvent, useEffect, useRef } from 'react';
 import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
-import dayjs from 'dayjs';
-import ReactDatePicker from 'react-datepicker';
-import { ko } from 'date-fns/locale';
-
 import { userFilterOptions } from 'states/userFilterOptions';
 import { userDataList } from 'states/userDataList';
 import { userSearchResult } from 'states/userSearchResult';
-import { Button } from 'components/common/Button';
 
+import dayjs from 'dayjs';
+
+import { Button } from 'components/common/Button';
+import SearchDatePicker from './SearchDatePicker';
 import './datePicker.scss';
 import styles from './searchForm.module.scss';
+import SearchInput from './SearchInput';
 
 interface Props {
   focusState: boolean;
 }
 
-const MIN_DATE = new Date(2022, 1, 1);
-const MAX_DATE = new Date(2022, 6, 30);
+const MIN_DATE = new Date(2022, 0, 1);
+const MAX_DATE = new Date(2022, 11, 31);
 
 function SearchFormInput({ focusState }: Props) {
   const userData = useRecoilValue(userDataList);
   const setSearchResult = useSetRecoilState(userSearchResult);
   const [filterOptions, setFilterOptions] = useRecoilState(userFilterOptions);
-
-  const [isStartBoxOpen, setIsStartBoxOpen] = useState(false);
-  const [isEndBoxOpen, setIsEndBoxOpen] = useState(false);
-
   const inputRef = useRef<HTMLInputElement>(null);
 
   const start = filterOptions.prevDate
@@ -46,56 +42,19 @@ function SearchFormInput({ focusState }: Props) {
 
   const onSubmitByEnter = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key !== 'Enter') return;
-
     const { loginId, id, prevDate, nextDate } = filterOptions;
-
+    const dataFilter = userData.filter(
+      (data) =>
+        (!loginId || data.loginId.includes(loginId)) &&
+        (!id || String(data.id).includes(id)) &&
+        (!prevDate || dayjs(prevDate) <= dayjs(data.registerDate)) &&
+        (!nextDate || dayjs(nextDate) >= dayjs(data.registerDate))
+    );
     if (!loginId && !id && !prevDate && !nextDate) {
       setSearchResult(userData);
       return;
     }
-
-    setSearchResult(
-      userData.filter(
-        (data) =>
-          (!loginId || data.loginId.includes(loginId)) &&
-          (!id || String(data.id).includes(id)) &&
-          (!prevDate || dayjs(prevDate) <= dayjs(data.registerDate)) &&
-          (!nextDate || dayjs(nextDate) >= dayjs(data.registerDate))
-      )
-    );
-  };
-
-  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFilterOptions((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const onChangeCalendarInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.currentTarget;
-
-    setFilterOptions((prev) => ({
-      ...prev,
-      [name]: value.length === 4 || value.length === 7 ? `${value}-` : value,
-    }));
-  };
-
-  const onFocusStartInput = () => {
-    setIsStartBoxOpen(true);
-  };
-
-  const onFocusEndInput = () => {
-    setIsEndBoxOpen(true);
-  };
-
-  const onBlurStartInput = () => {
-    setIsStartBoxOpen(false);
-  };
-
-  const onBlurEndInput = () => {
-    setIsEndBoxOpen(false);
+    setSearchResult(dataFilter);
   };
 
   const onChangeStartCalendar = (selectedDate: Date) => {
@@ -111,8 +70,6 @@ function SearchFormInput({ focusState }: Props) {
         prevDate: dayjs(selectedDate).format('YYYY-MM-DD'),
       }));
     }
-
-    setIsStartBoxOpen(false);
   };
 
   const onChangeEndCalendar = (selectedDate: Date) => {
@@ -128,22 +85,16 @@ function SearchFormInput({ focusState }: Props) {
         nextDate: dayjs(selectedDate).format('YYYY-MM-DD'),
       }));
     }
-
-    setIsEndBoxOpen(false);
   };
-
   return (
     <dl className={styles.inputBox}>
       <div className={styles.inputList}>
         <dt className={styles.loginIdLabel}>로그인ID</dt>
         <dd className={styles.loginIdDesc}>
-          <input
-            name="loginId"
-            type="text"
-            placeholder="전체"
-            onChange={onChangeInput}
-            value={filterOptions.loginId}
-            ref={inputRef}
+          <SearchInput
+            inputName="loginId"
+            inputValue={filterOptions.loginId}
+            focusRef={inputRef}
             onKeyDown={onSubmitByEnter}
           />
         </dd>
@@ -151,12 +102,11 @@ function SearchFormInput({ focusState }: Props) {
       <div className={styles.inputList}>
         <dt className={styles.loginNumberLabel}>회원번호</dt>
         <dd className={styles.loginNumberDesc}>
-          <input
-            name="id"
+          <SearchInput
+            inputName="id"
             type="number"
-            placeholder="전체"
-            onChange={onChangeInput}
-            value={filterOptions.id}
+            inputValue={filterOptions.id}
+            focusRef={inputRef}
             onKeyDown={onSubmitByEnter}
           />
         </dd>
@@ -164,69 +114,21 @@ function SearchFormInput({ focusState }: Props) {
       <div className={styles.inputList}>
         <dt className={styles.searchDateLabel}>조회기간</dt>
         <dd className={styles.searchDateDesc}>
-          <input
-            readOnly
-            name="prevDate"
-            type="text"
-            placeholder="전체"
-            onChange={onChangeCalendarInput}
-            value={filterOptions.prevDate}
+          <SearchDatePicker
+            onChange={onChangeStartCalendar}
             onKeyDown={onSubmitByEnter}
-            className={styles.startDateInput}
-            onFocus={onFocusStartInput}
+            selected={start}
           />
-          {isStartBoxOpen && (
-            <div className="datePicker">
-              <ReactDatePicker
-                inline
-                dateFormat="yyyy/MM/dd"
-                dateFormatCalendar="yyyy LLLL"
-                selected={start}
-                onChange={onChangeStartCalendar}
-                selectsStart
-                startDate={start}
-                endDate={end}
-                minDate={MIN_DATE}
-                maxDate={MAX_DATE}
-                locale={ko}
-                onClickOutside={onBlurStartInput}
-              />
-            </div>
-          )}
         </dd>
         <dd className={styles.fromTo}>~</dd>
         <dd className={styles.searchDateDesc}>
-          <input
-            readOnly
-            name="nextDate"
-            type="text"
-            placeholder="전체"
-            onChange={onChangeCalendarInput}
-            value={filterOptions.nextDate}
-            className={styles.endDateInput}
+          <SearchDatePicker
+            onChange={onChangeEndCalendar}
             onKeyDown={onSubmitByEnter}
-            onFocus={onFocusEndInput}
+            selected={end}
           />
-          {isEndBoxOpen && (
-            <div className="datePicker">
-              <ReactDatePicker
-                inline
-                dateFormat="yyyy/MM/dd"
-                dateFormatCalendar="yyyy LLLL"
-                selected={end}
-                onChange={onChangeEndCalendar}
-                selectsEnd
-                startDate={start}
-                endDate={end}
-                minDate={MIN_DATE}
-                maxDate={MAX_DATE}
-                locale={ko}
-                onClickOutside={onBlurEndInput}
-              />
-            </div>
-          )}
         </dd>
-        <dd className={styles.datePicker}>
+        <dd className={styles.datePickerCategory}>
           <Button className={styles.today} size="short">
             오늘
           </Button>
